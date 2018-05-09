b2ff09125c309f10a7b3580c606840292cb6a319
holyshared <holy.shared.design@gmail.com>
2018-05-09 11:23:16 +0900

    fix format

--- example/app.hh
+++ example/app.hh
@@ -2,49 +2,62 @@
 
 namespace HHPack\Getopt\Example;
 
-require_once __DIR__ . '/../vendor/hh_autoload.php';
+require_once __DIR__.'/../vendor/hh_autoload.php';
 
 use HHPack\Getopt as cli;
-use HHPack\Getopt\App\{ ArgumentParser };
+use HHPack\Getopt\App\{ArgumentParser};
 
-final class CliApplication
-{
+final class CliApplication {
 
-    private bool $help = false;
-    private bool $version = false;
-    private string $fileName = 'test';
-    private ArgumentParser $argParser;
+  private bool $help = false;
+  private bool $version = false;
+  private string $fileName = 'test';
+  private ArgumentParser $argParser;
 
-    public function __construct()
-    {
-        $this->argParser = cli\app('example', '1.0.0')
-            ->description("This cli application is example.\n\n")
-            ->usage("  {app.name} [OPTIONS]\n\n")
-            ->options([
-                cli\take_on(['-n', '--name'], 'NAME', 'file name', ($name) ==> {
-                    $this->fileName = $name;
-                }),
-                cli\on(['-h', '--help'], 'display help message', () ==> {
-                    $this->help = true;
-                }),
-                cli\on(['-v', '--version'], 'display version', () ==> {
-                    $this->version = true;
-                })
-            ]);
+  public function __construct() {
+    $this->argParser =
+      cli\app('example', '1.0.0')
+        ->description("This cli application is example.\n\n")
+        ->usage("  {app.name} [OPTIONS]\n\n")
+        ->options(
+          [
+            cli\take_on(
+              ['-n', '--name'],
+              'NAME',
+              'file name',
+              ($name) ==> {
+                $this->fileName = $name;
+              },
+            ),
+            cli\on(
+              ['-h', '--help'],
+              'display help message',
+              () ==> {
+                $this->help = true;
+              },
+            ),
+            cli\on(
+              ['-v', '--version'],
+              'display version',
+              () ==> {
+                $this->version = true;
+              },
+            ),
+          ],
+        );
+  }
+
+  public function run(Traversable<string> $argv): void {
+    $this->argParser->parse($argv);
+
+    if ($this->help) {
+      $this->argParser->displayHelp();
+    } else if ($this->version) {
+      $this->argParser->displayVersion();
+    } else {
+      echo "file name: ", $this->fileName, PHP_EOL;
     }
-
-    public function run(Traversable<string> $argv): void
-    {
-        $this->argParser->parse($argv);
-
-        if ($this->help) {
-            $this->argParser->displayHelp();
-        } else if ($this->version) {
-            $this->argParser->displayVersion();
-        } else {
-            echo "file name: ", $this->fileName, PHP_EOL;
-        }
-    }
+  }
 }
 
 (new CliApplication())->run($argv);
--- example/example.hh
+++ example/example.hh
@@ -2,38 +2,58 @@
 
 namespace HHPack\Getopt\Example;
 
-require_once __DIR__ . '/../vendor/hh_autoload.php';
+require_once __DIR__.'/../vendor/hh_autoload.php';
 
 use HHPack\Getopt as cli;
 
 final class Options {
-    public bool $help = false;
-    public bool $version = false;
-    public string $fileName = 'test';
+  public bool $help = false;
+  public bool $version = false;
+  public string $fileName = 'test';
 }
 
-function optparser_main(Traversable<string> $argv) : void
-{
-    $options = new Options();
+function optparser_main(Traversable<string> $argv): void {
+  $options = new Options();
 
-    $parser = cli\optparser([
-        cli\take_on([ '-n', '--name' ], 'NAME', 'file name', ($name) ==> { $options->fileName = $name; }),
-        cli\on([ '-h', '--help' ], 'display help message', () ==> { $options->help = true; }),
-        cli\on([ '-v', '--version' ], 'display version', () ==> { $options->version = true; })
-    ]);
+  $parser = cli\optparser(
+    [
+      cli\take_on(
+        ['-n', '--name'],
+        'NAME',
+        'file name',
+        ($name) ==> {
+          $options->fileName = $name;
+        },
+      ),
+      cli\on(
+        ['-h', '--help'],
+        'display help message',
+        () ==> {
+          $options->help = true;
+        },
+      ),
+      cli\on(
+        ['-v', '--version'],
+        'display version',
+        () ==> {
+          $options->version = true;
+        },
+      ),
+    ],
+  );
 
-    $args = $parser->parse($argv);
+  $args = $parser->parse($argv);
 
-    if ($options->help === true) {
-        echo 'help on', PHP_EOL;
-    }
+  if ($options->help === true) {
+    echo 'help on', PHP_EOL;
+  }
 
-    if ($options->version === true) {
-        echo 'version on', PHP_EOL;
-    }
+  if ($options->version === true) {
+    echo 'version on', PHP_EOL;
+  }
 
-    if ($options->fileName !== 'test') {
-        echo 'name = ', $options->fileName, PHP_EOL;
-    }
+  if ($options->fileName !== 'test') {
+    echo 'name = ', $options->fileName, PHP_EOL;
+  }
 }
 optparser_main($argv);
